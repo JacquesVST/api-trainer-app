39c657eeee45d5897833ec244b06eb13c91947af
Jacques <jacquesvst@gmail.com>
2021-02-23 23:25:30 -0300

    Breaking changes and overall refactoring

--- src/main/java/com/jvst/api/imagem/repository/ImagemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jvst.api.imagem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jvst.api.imagem.model.Imagem;
-
-public interface ImagemRepository extends JpaRepository<Imagem, Long> { 
-
-}
--- src/main/java/com/jvst/api/video/repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jvst.api.video.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jvst.api.video.model.Video;
-
-public interface VideoRepository extends JpaRepository<Video, Long>{
-
-}
